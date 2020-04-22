cf2682a01dc810c3ee7731dd3dfb2a0d1482ad63
fandong <admin@fandong.me>
2020-04-22 11:45:42 +0800

    1.0.6

--- FDUIKitObjC/Classes/Define/FDUIDefine.h
+++ FDUIKitObjC/Classes/Define/FDUIDefine.h
@@ -14,6 +14,7 @@
 #define FD_Bottom_SafeArea_Height ([[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0 ? 34 : 0)
 #define FD_StatusBar_Height [UIApplication sharedApplication].statusBarFrame.size.height
 #define FD_Navigation_Height 44
+#define FD_Bottom_Height ([[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0 ? 83 : 49)
 #define FD_Top_Height [UIApplication sharedApplication].statusBarFrame.size.height + FD_Navigation_Height
 
 #endif /* FDUIDefine_h */
